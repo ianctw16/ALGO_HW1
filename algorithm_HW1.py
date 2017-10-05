72d5df697ae80cd5ea296e1cdfb51f6a4941472b
ianctw16 <ian08160816@hotmail.com>
2017-10-05 23:35:45 +0800

    13

--- algorithm_HW1.py
+++ algorithm_HW1.py
@@ -62,33 +62,3 @@ s.write(str(result_time))
 s.write('\n\n******************************************************************\n\n')
 s.write(str(score))
 s.close()
-"""
-for word in regeTokened:
-    # print(word, len(word))
-    tokened_Len.append(len(word))
-     # tokened_Len.append(len(word))
-     # print(j)
-     # j = j + 1
-
-# print(tokened_Len)
-# print(len(tokened_Len))
-
-score = []
-tmp = 0
-
-for i in range(0, len(tokened_Len)):
-    j = i + 1
-    tmp = 0
-    while(j < len(tokened_Len)):
-        if(tokened_Len[j] > tokened_Len[i]):
-            tmp = tmp + 1
-            j = j + 1
-        else:
-            j = j + 1
-    score.append(tmp)
-
-print(time.time() - start)
-print("******************************************************************")
-print(score)
-# print(len(score))
-"""
